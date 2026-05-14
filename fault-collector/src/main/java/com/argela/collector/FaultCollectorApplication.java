8dd0e7213e68ba228a50850dc3648d468938e0fa
mesmatechhavasavunma <mesmatechhavasavunma@gmail.com>
2026-05-14 11:45:01 +0300

    fault-collector iskeleti

---
+++ fault-collector/src/main/java/com/argela/collector/FaultCollectorApplication.java
@@ -0,0 +1,12 @@
+package com.argela.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaultCollectorApplication.class, args);
+    }
+}
